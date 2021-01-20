082683781d8ccfa2826a3b58d9ba352c3bd54624
Andreas Linde <andylindebros@gmail.com>
2021-01-20 14:27:07 +0100

    Added sync call

--- Sources/LindebrosApiClient/LindebrosApiClient.swift
+++ Sources/LindebrosApiClient/LindebrosApiClient.swift
@@ -19,28 +19,75 @@ public struct LindebrosApiClient {
 public extension LindebrosApiClient {
     /**
      Makes a request to the API.
-
      - parameter r: The request object that describes the API request
+     - parameter bearerToken: The user token to authenticate the request with
      - parameter completionHandler: The callback method to be invoked when the request is completed
      */
     func call<Model: Decodable, ErrorModel: ErrorResponse, RequestBodyModel: Encodable>(
         _ r: Request<Model, ErrorModel, RequestBodyModel>,
-        bearerToken: String,
+        bearerToken: String? = nil,
         completionHandler: @escaping (_ result: ApiResponse<Model, ErrorModel>) -> Void
     ) {
         let session = URLSession.shared
-        let endpointUrlStr = "\(r.isRelativeUrl ? baseURL : "")\(r.endpoint)"
-        guard let endpointUrl = URL(string: endpointUrlStr) else {
+        guard let request = createURLRequest(r: r, bearerToken: bearerToken) else {
             completionHandler(
                 ApiResponse(isOk: false, status: HTTPStatusCode.badRequest, data: nil, error: nil)
             )
             return
         }
+
+        let task = session.dataTask(with: request, completionHandler: { data, response, requestError in
+
+            let responseObject = bakeResponse(r: r, data: data, response: response, requestError: requestError)
+
+            // We will continue to work on a background thread and let ApiClient switch back to the main thread.
+            DispatchQueue.main.async {
+                completionHandler(responseObject)
+            }
+        })
+
+        task.resume()
+    }
+
+    /**
+     Makes a synchronious request to the API.
+     - parameter r: The request object that describes the API request
+     - parameter bearerToken: The user token to authenticate the request with
+     - returns: An object representing the response of the request
+     */
+    func syncCall<Model: Decodable, ErrorModel: ErrorResponse, RequestBodyModel: Encodable>(
+        _ r: Request<Model, ErrorModel, RequestBodyModel>,
+        bearerToken: String? = nil
+    ) -> ApiResponse<Model, ErrorModel> {
+        let semaphore = DispatchSemaphore(value: 0)
+
+        var responseObject: ApiResponse<Model, ErrorModel>!
+
+        call(r, bearerToken: bearerToken) { response in
+            responseObject = response
+            semaphore.signal()
+        }
+        _ = semaphore.wait(wallTimeout: .distantFuture)
+
+        return responseObject
+    }
+}
+
+private extension LindebrosApiClient {
+    func createURLRequest<Model, ErrorModel, RequestBodyModel>(r: Request<Model, ErrorModel, RequestBodyModel>, bearerToken: String?) -> URLRequest? {
+        let endpointUrlStr = "\(r.isRelativeUrl ? baseURL : "")\(r.endpoint)"
+        guard let endpointUrl = URL(string: endpointUrlStr) else {
+            return nil
+        }
+
         var request = URLRequest(url: endpointUrl)
         request.httpMethod = r.method.rawValue
         request.timeoutInterval = 6
         request.addValue(r.contentType.header, forHTTPHeaderField: "Content-Type")
-        request.addValue("Bearer " + bearerToken, forHTTPHeaderField: "Authorization")
+
+        if let bearerToken = bearerToken {
+            request.addValue("Bearer " + bearerToken, forHTTPHeaderField: "Authorization")
+        }
 
         if let customHeaders = r.customHeaders {
             customHeaders.forEach { customHeader in
@@ -56,54 +103,51 @@ public extension LindebrosApiClient {
         let requestStr = "[\(r.method.rawValue)] \(endpointUrlStr) \(String(data: request.httpBody ?? Data(), encoding: .utf8) ?? "")"
         logger.info(requestStr)
 
-        let task = session.dataTask(with: request, completionHandler: { data, response, requestError in
-            let response = response as? HTTPURLResponse
-            let httpStatus: HTTPStatusCode = HTTPStatusCode(rawValue: response?.statusCode ?? 0) ?? .unknown
+        return request
+    }
 
-            var model: Model?
+    func bakeResponse<Model, ErrorModel, RequestBodyModel>(r: Request<Model, ErrorModel, RequestBodyModel>, data: Data?, response: URLResponse?, requestError: Error?) -> ApiResponse<Model, ErrorModel> {
+        let response = response as? HTTPURLResponse
+        let httpStatus: HTTPStatusCode = HTTPStatusCode(rawValue: response?.statusCode ?? 0) ?? .unknown
 
-            var errorModel: ErrorModel?
+        var model: Model?
 
-            var errors: [Error] = []
-            if let error = requestError {
-                errors.append(error)
-            }
+        var errorModel: ErrorModel?
 
-            var isOk = httpStatus.isOk()
-            if let data = data, data.count > 0 {
-                let jsonDecoder = JSONDecoder()
-                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
-                do {
-                    if httpStatus.isOk() {
-                        model = try jsonDecoder.decode(Model.self, from: data)
-                    } else {
-                        errorModel = try jsonDecoder.decode(ErrorModel.self, from: data)
-                    }
-                } catch let e {
-                    // Logger.error("\(endpointUrlStr): Could not convert model \(e)")
-                    isOk = false
-                    errors.append(e)
+        var errors: [Error] = []
+        if let error = requestError {
+            errors.append(error)
+        }
+
+        var isOk = httpStatus.isOk()
+        if let data = data, data.count > 0 {
+            let jsonDecoder = JSONDecoder()
+            jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+            do {
+                if httpStatus.isOk() {
+                    model = try jsonDecoder.decode(Model.self, from: data)
+                } else {
+                    errorModel = try jsonDecoder.decode(ErrorModel.self, from: data)
                 }
+            } catch let e {
+                // Logger.error("\(endpointUrlStr): Could not convert model \(e)")
+                isOk = false
+                errors.append(e)
+            }
+        }
+
+        if errors.count > 0 {
+            errors.forEach { e in
+                logger.error("\(e)")
             }
 
-            if errors.count > 0 {
-                errors.forEach { e in
-                    logger.error("\(e)")
-                }
+            errorModel = ErrorMessage(message: errors.first?.localizedDescription) as? ErrorModel
+        }
 
-                errorModel = ErrorMessage(message: errors.first?.localizedDescription) as? ErrorModel
-            }
+        let responseObject = ApiResponse(isOk: isOk, status: httpStatus, data: model, errorModel: errorModel, error: errors)
 
-            let responseObject = ApiResponse(isOk: isOk, status: httpStatus, data: model, errorModel: errorModel, error: errors)
+        logger.info("\(r.endpoint) \(errors.count == 0 ? responseObject.status.getEmoj() : "⚠️") \(responseObject.status.rawValue) \(!responseObject.isOk ? String(data: data ?? Data(), encoding: .utf8) ?? "" : r.debugData ? String(data: data ?? Data(), encoding: .utf8) ?? "" : "")")
 
-            logger.info("\(r.endpoint) \(errors.count == 0 ? responseObject.status.getEmoj() : "⚠️") \(responseObject.status.rawValue) \(!responseObject.isOk ? String(data: data ?? Data(), encoding: .utf8) ?? "" : r.debugData ? String(data: data ?? Data(), encoding: .utf8) ?? "" : "")")
-
-            // We will continue to work on a background thread and let ApiClient switch back to the main thread.
-            DispatchQueue.main.async {
-                completionHandler(responseObject)
-            }
-        })
-
-        task.resume()
+        return responseObject
     }
 }
